dfbe8b602ad8513f0bbe3ccd2c7ab48a63a500ff
Arnav <arnav.gohil04@gmail.com>
2020-02-16 12:50:13 +0530

    Commit

--- app/src/main/java/com/example/mltrial2/PreviewActivity.java
+++ app/src/main/java/com/example/mltrial2/PreviewActivity.java
@@ -1,32 +1,198 @@
 package com.example.mltrial2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.Matrix;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.common.FirebaseMLException;
+import com.google.firebase.ml.common.modeldownload.FirebaseLocalModel;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelManager;
+import com.google.firebase.ml.common.modeldownload.FirebaseRemoteModel;
+import com.google.firebase.ml.custom.FirebaseModelDataType;
+import com.google.firebase.ml.custom.FirebaseModelInputOutputOptions;
+import com.google.firebase.ml.custom.FirebaseModelInputs;
+import com.google.firebase.ml.custom.FirebaseModelInterpreter;
+import com.google.firebase.ml.custom.FirebaseModelOptions;
+import com.google.firebase.ml.custom.FirebaseModelOutputs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class PreviewActivity extends AppCompatActivity {
+
+    Bitmap bitmap;
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preview);
 
+        txt = findViewById(R.id.textView);
 
+        ImageView imageView = findViewById(R.id.imageView);
 
-        ImageView imageView=findViewById(R.id.imageView);
-        
-        Bitmap bmp = BitmapFactory.decodeFile(String.valueOf(MainActivity.file));
+        bitmap = BitmapFactory.decodeFile(String.valueOf(MainActivity.file));
 
         Matrix matrix = new Matrix();
 
         matrix.postRotate(90);
 
-        Bitmap rotatedBitmap = Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(), bmp.getHeight(), matrix, true);
+        Bitmap rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
 
         imageView.setImageBitmap(rotatedBitmap);
+
+
+        // [START mlkit_cloud_model_source]
+        FirebaseModelDownloadConditions.Builder conditionsBuilder =
+                new FirebaseModelDownloadConditions.Builder().requireWifi();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+// Enable advanced conditions on Android Nougat and newer.
+            conditionsBuilder = conditionsBuilder
+                    .requireCharging()
+                    .requireDeviceIdle();
+        }
+        FirebaseModelDownloadConditions conditions = conditionsBuilder.build();
+// Build a remote model source object by specifying the name you assigned the model
+// when you uploaded it in the Firebase console.
+        FirebaseRemoteModel cloudSource = new FirebaseRemoteModel.Builder("flowers")
+                .enableModelUpdates(true)
+                .setInitialDownloadConditions(conditions)
+                .setUpdatesDownloadConditions(conditions)
+                .build();
+        FirebaseModelManager.getInstance().registerRemoteModel(cloudSource);
+// [END mlkit_cloud_model_source]
+
+
+        // [START mlkit_local_model_source]
+        FirebaseLocalModel localSource =
+                new FirebaseLocalModel.Builder("my_local_model")  // Assign a name to this model
+                        .setAssetFilePath("model.tflite")
+                        .build();
+        FirebaseModelManager.getInstance().registerLocalModel(localSource);
+// [END mlkit_local_model_source]
+
+
+        try {
+            runInference();
+        } catch (FirebaseMLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private FirebaseModelInterpreter createInterpreter() throws FirebaseMLException {
+// [START mlkit_create_interpreter]
+        FirebaseModelOptions options = new FirebaseModelOptions.Builder()
+                .setRemoteModelName("flowers")
+                .setLocalModelName("my_local_model")
+                .build();
+        FirebaseModelInterpreter firebaseInterpreter =
+                FirebaseModelInterpreter.getInstance(options);
+// [END mlkit_create_interpreter]
+        return firebaseInterpreter;
+    }
+
+    private FirebaseModelInputOutputOptions createInputOutputOptions() throws FirebaseMLException {
+// [START mlkit_create_io_options]
+        FirebaseModelInputOutputOptions inputOutputOptions =
+                new FirebaseModelInputOutputOptions.Builder()
+                        .setInputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1, 224, 224, 3})
+                        .setOutputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1, 5})
+                        .build();
+// [END mlkit_create_io_options]
+        return inputOutputOptions;
+    }
+
+    private float[][][][] bitmapToInputArray() {
+// [START mlkit_bitmap_input]
+        bitmap = Bitmap.createScaledBitmap(bitmap, 224, 224, true);
+        int batchNum = 0;
+        float[][][][] input = new float[1][224][224][3];
+        for (int x = 0; x < 224; x++) {
+            for (int y = 0; y < 224; y++) {
+                int pixel = bitmap.getPixel(x, y);
+// Normalize channel values to [-1.0, 1.0]. This requirement varies by
+// model. For example, some models might require values to be normalized
+// to the range [0.0, 1.0] instead.
+                input[batchNum][x][y][0] = (Color.red(pixel) - 127) / 128.0f;
+                input[batchNum][x][y][1] = (Color.green(pixel) - 127) / 128.0f;
+                input[batchNum][x][y][2] = (Color.blue(pixel) - 127) / 128.0f;
+            }
+        }
+// [END mlkit_bitmap_input]
+        return input;
+    }
+
+    private void runInference() throws FirebaseMLException {
+        FirebaseModelInterpreter firebaseInterpreter = createInterpreter();
+        float[][][][] input = bitmapToInputArray();
+        FirebaseModelInputOutputOptions inputOutputOptions = createInputOutputOptions();
+// [START mlkit_run_inference]
+        FirebaseModelInputs inputs = new FirebaseModelInputs.Builder()
+                .add(input)  // add() as many input arrays as your model requires
+                .build();
+        firebaseInterpreter.run(inputs, inputOutputOptions)
+                .addOnSuccessListener(
+                        new OnSuccessListener<FirebaseModelOutputs>() {
+                            @Override
+                            public void onSuccess(FirebaseModelOutputs result) {
+// [START_EXCLUDE]
+// [START mlkit_read_result]
+                                float[][] output = result.getOutput(0);
+                                float[] probabilities = output[0];
+                                try {
+                                    useInferenceResult(probabilities);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
+// [END mlkit_read_result]
+// [END_EXCLUDE]
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+// Task failed with an exception
+// ...
+                            }
+                        });
+// [END mlkit_run_inference]
+    }
+
+    private void useInferenceResult(float[] probabilities) throws IOException {
+// [START mlkit_use_inference_result]
+
+        String maxi = null;
+        float maxP = Float.MIN_VALUE;
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(getAssets().open("labels.txt")));
+        for (int i = 0; i < probabilities.length; i++) {
+            String label = reader.readLine();
+            if (probabilities[i] > maxP) {
+                maxi = label;
+                maxP = probabilities[i];
+            }
+            Log.i("MLKit", String.format("%s: %1.4f", label, probabilities[i]));
+        }
+        maxP = maxP*100;
+        txt.setText(maxi + " " + maxP + "%");
+// [END mlkit_use_inference_result]
     }
 }
